7a9d0d7b6331dd84a5c1f8e14382f0ef28817687
roboanonymous <jsamyak591@gmail.com>
2023-03-30 10:47:07 +0530

    DFS

--- src/Searching/DFS_Algorithm.java
+++ src/Searching/DFS_Algorithm.java
@@ -1,14 +1,13 @@
 package Searching;
 
-
 import java.util.*;
 
-public class BFS_Algorithm {
+public class DFS_Algorithm {
 
     private int V;
     private LinkedList<Integer> adj[];
 
-    BFS_Algorithm(int v)
+    DFS_Algorithm(int v)
     {
         V = v;
         adj = new LinkedList[v];
@@ -18,36 +17,37 @@ public class BFS_Algorithm {
 
 
     void addEdge(int v, int w) { adj[v].add(w); }
-    void BFS(int s)
+    void DFS(int s)
     {
-        Queue <Integer> q = new LinkedList<>();
-        boolean visited[]  = new boolean[V];
-        System.out.println(visited);
-        System.out.println(Arrays.toString(visited));
+        Stack<Integer> stack = new Stack<>();
+        boolean visited[] = new boolean[V];
+        stack.push(s);
         visited[s] = true;
-        q.add(s);
 
-        while(!q.isEmpty())
+        while (!stack.isEmpty())
         {
-            s = q.poll();
+            s = stack.pop();
             System.out.print(s+ " ");
 
             Iterator<Integer> i = adj[s].listIterator();
-            while (i.hasNext()) {
+            while (i.hasNext())
+            {
                 int n = i.next();
-                if (!visited[n]) {
+                if(!visited[n])
+                {
                     visited[n] = true;
-                    q.add(n);
-                }
+                    stack.push(n);
 
+                }
             }
         }
+
     }
 
 
     public static void main(String args[])
     {
-        BFS_Algorithm g = new BFS_Algorithm(4);
+        DFS_Algorithm g = new DFS_Algorithm(4);
 
         g.addEdge(0, 1);
         g.addEdge(0, 2);
@@ -57,10 +57,10 @@ public class BFS_Algorithm {
         g.addEdge(3, 3);
 
         System.out.println(
-                "Following is Breadth First Traversal "
+                "Following is Depth First Traversal "
                         + "(starting from vertex 2)");
 
-        g.BFS(2);
+        g.DFS(2);
     }
 
 }
